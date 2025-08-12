96a27304ae4b426f42b48e71336e8b53a9166d03
shaistaDev7 <shaistazulfiqar61@gmail.com>
2025-08-12 23:51:55 +0500

    add Class_1c and data file

---
+++ Class_1c.R
@@ -0,0 +1,12 @@
+#Class 1c practical
+#Variables
+gene<-"TP53"
+gene
+#Rules for defining variable
+# 1. Don't allow space between names
+# 2. Names of variable must be start from letter not a number
+# 3. R is case sensitive
+
+#To import data as variable
+raw_data<-read.csv(file.choose())
+raw_data
